bec45c169b0aae588f8e2ca39aaf3c046f2822db
Adrián Martínez Fuentes <UO295454@uniovi.es>
2024-06-01 15:46:29 +0200

    Copying files to modify from html-css version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable eqeqeq */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import './App.css';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import CreateUserComp from './Components/CreateUserComp';
+import LoginUserComp from './Components/LoginUserComp';
+import React, { useEffect, useState } from 'react';
+import { backendUrl } from './Globals';
+import CreatePresentComp from './Components/CreatePresentComp';
+import MyPresentsComp from './Components/MyPresentsComp';
+import EditPresentComp from './Components/EditPresentComp';
+import AddFriendComp from './Components/AddFriendComp';
+import MyFriendsComp from './Components/MyFriendsComp';
+import GivePresentComp from './Components/GivePresentComp';
 
 function App() {
+  let [notif, setNotif] = useState("")
+  let [login, setLogin] = useState(false)
+
+  let navigate = useNavigate()
+
+  useEffect(() => {
+    navigate("/")
+    //setLogin(false)
+  }, [])  
+  
+  let createNotif = (msg) => {
+    setNotif(msg)
+    setTimeout(() => {setNotif("")}, 3000)
+  }
+
+  let disconnect = async () => {
+    await fetch(backendUrl + "/users/disconnect?apiKey=" + localStorage.getItem("apiKey"))
+    localStorage.removeItem("apiKey")
+    setLogin(false)
+    navigate("/login")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <nav>
+        <ul className='navbar'>
+          <li><Link to="/">Index</Link></li>
+          {!login && <li><Link to="/register">Register</Link></li>}
+          {!login && <li><Link to="/login">Log In</Link></li>}
+          {login && <li><Link to="/presents">Create Present</Link></li>}  
+          {login && <li><Link to="/myPresents">My Presents</Link></li>}   
+          {login && <li><Link to="/addFriend">Add friend</Link></li>}  
+          {login && <li><Link to="/myFriends">My friends</Link></li>}  
+          {login && <li><Link to="/givePresent">Give present!</Link></li>}  
+          {login && <li><Link to="#" onClick={disconnect}>Disconnect</Link></li>}  
+        </ul>
+      </nav>
+
+      {notif != "" && (
+        <div className='notification'>
+          {notif}
+          <span className='close-btn' onClick={() => {setNotif("")}}>X</span>
+        </div>
+      )}
+      
+      <Routes>
+        <Route path="/" element={<p>Index of website</p>}/>
+        <Route path="/register" element={<CreateUserComp createNotification={createNotif}/>}/>
+        <Route path="/login" element={<LoginUserComp setLogin={setLogin}/>}/>
+        <Route path="/presents" element={<CreatePresentComp createNotification={createNotif} />}/>
+        <Route path="/myPresents" element={<MyPresentsComp createNotification={createNotif} />}/>
+        <Route path="/edit/:presentId" element={<EditPresentComp createNotification={createNotif} />}/>
+        <Route path="/addFriend" element={<AddFriendComp createNotification={createNotif} />}/>
+        <Route path="/myFriends" element={<MyFriendsComp createNotification={createNotif} />}/>
+        <Route path="/givePresent" element={<GivePresentComp createNotification={createNotif} />}/>
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/MyPresentsComp.js
@@ -0,0 +1,83 @@
+/* eslint-disable eqeqeq */
+import { useEffect, useState } from "react"
+import { Link, useNavigate, } from "react-router-dom"
+import { backendUrl } from "../Globals"
+
+let MyPresentsComp = (props) => {
+    let {createNotification} = props
+
+    let [presents, setPresents] = useState([])
+    let [message, setMessage] = useState("")
+
+    let navigate = useNavigate()
+
+    useEffect(() => {
+        getPresens()
+    }, [])
+
+    let getPresens = async () => {
+        let response = await fetch(backendUrl + "/presents?apiKey=" + localStorage.getItem("apiKey"), {
+            method: "GET",
+            headers: {"Content-Type" : "application/json"}
+        })
+    
+        if(response.ok)
+        {
+            let jsonData = await response.json()
+            setPresents(jsonData)
+        }
+        else
+        {
+            let jsonData = await response.json()
+            setMessage(jsonData.error)
+        }
+    }
+
+    let onClickDeleteItem = async (id) => {
+        let response = await fetch(backendUrl + "/presents/" + id + "?apiKey=" 
+            + localStorage.getItem("apiKey"), { method : "DELETE" })
+        
+        if(response.ok)
+        {
+            let updatedPresents = presents.filter(present => present.id != id)
+            setPresents(updatedPresents)
+            createNotification("Present succesfully deleted")
+        }
+        else
+        {
+            let jsonData = await response.json()
+            setMessage(jsonData.error)
+        }
+    }
+
+    let onClickEditItem = (id) => {
+        navigate("/edit/" + id)
+    }
+
+    return (
+        <div>
+            <h2>My Presents</h2>
+            {message !== "" && <h3 className="errorMessage">{message}</h3>}
+
+            <div className="item-list">
+                { presents.map (present => 
+                    (
+                        <div className="item">
+                            <h3 className="name">{present.name}</h3>
+                            <h3 className="description">Description: {present.description}</h3>
+                            <h3 className="url">URL: {present.url}</h3>
+                            <h3 className="price">Price: {present.price} €</h3>
+                            <h3 className="choosenBy">
+                                Choosen by: {present.choosenBy === "" 
+                                    || present.choosenBy == null ? "No one" : present.choosenBy}
+                            </h3>                                    
+                            <button onClick={() => {onClickEditItem(present.id)}}>Edit Item</button>
+                            <button onClick={() => {onClickDeleteItem(present.id)}}>Delete Item</button>   
+                        </div>                                      
+                    )
+                )}
+            </div>
+        </div>
+    )
+}
+export default MyPresentsComp;
